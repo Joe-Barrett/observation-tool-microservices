42570f8dbc25fbe861be3e7c3da1b34d671fb3aa
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-05-08 15:48:55 +0100

    Implement project resource

--- project-service/src/main/java/com/prototype/ot/microservices/projectservice/resources/ProjectResource.java
+++ project-service/src/main/java/com/prototype/ot/microservices/projectservice/resources/ProjectResource.java
@@ -1,8 +1,15 @@
 package com.prototype.ot.microservices.projectservice.resources;
 
+import com.prototype.ot.microservices.projectservice.model.project.ObsProject;
 import com.prototype.ot.microservices.projectservice.services.ProjectService;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class ProjectResource {
@@ -13,12 +20,24 @@ public class ProjectResource {
         this.projectService = new ProjectService();
     }
 
+    @GetMapping
     public ResponseEntity getAllProjects() {
-        return ResponseEntity.ok().build();
+        try {
+            List<ObsProject> projects = this.projectService.getAllProjects();
+            return ResponseEntity.ok(projects);
+        } catch (IOException ex) {
+            return ResponseEntity.status(404).body(ex.getMessage());
+        }
     }
 
-    public ResponseEntity getProposal(String proposalRef) {
-        return ResponseEntity.ok().build();
+    @GetMapping(path = "/proposal")
+    public ResponseEntity getProposal(@RequestParam(value = "proposalRef", required = false) String proposalRef) {
+        try {
+            JSONObject foundProposal = this.projectService.getProposal(proposalRef);
+            return ResponseEntity.ok(foundProposal);
+        } catch (IOException ex) {
+            return ResponseEntity.status(404).body(ex.getMessage());
+        }
     }
 
 }
